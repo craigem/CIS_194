7c1e04b1cf3d04f595b0798dcca8b83e8a6504b1
Craige McWhirter <craige@mcwhirter.com.au>
2014-07-17 20:05:19 +1000

    Made a start on Ex1

---
+++ 01/exercise_1.hs
@@ -0,0 +1,11 @@
+-- CIS 194, Homework 1, Exercise 1
+
+-- Example: toDigits 1234 == [1,2,3,4]
+-- Example: toDigitsRev 1234 == [4,3,2,1]
+-- Example: toDigits 0 == []
+-- Example: toDigits (-17) == []
+
+toDigits    :: Integer -> [Integer]
+
+toDigitsRev :: Integer -> [Integer]
+
